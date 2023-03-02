3f4c99e205f83613fc6742afbb59a818280a786e
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-03-02 14:56:04 -0700

    Update App.js
    Added a some logic in order to display to the user some indication that data is being fetched.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
     setMovies(data.results);
+    setIsLoading(false);
   };
 
   return (
@@ -18,10 +21,11 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
-}
+};
 
 export default App;
